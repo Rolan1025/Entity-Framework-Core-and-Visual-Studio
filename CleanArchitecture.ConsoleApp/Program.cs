1bb830a5d5179a38777d5c65b6a730b600aca80a
Orlando Delgado Pinzon <95199295+Rolan1025@users.noreply.github.com>
2024-02-22 08:47:44 -0500

    Update Program.cs

--- CleanArchitecture.ConsoleApp/Program.cs
+++ CleanArchitecture.ConsoleApp/Program.cs
@@ -4,8 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using System.Diagnostics.Metrics;
 
 StreamerDbContext dbContext = new();
-await ImprimeDigitos2();
-//await ImprimeDigitos();
 //await MultipleEntitiesQuery();
 //await AddNewDirectorWithVideo();
 //await AddNewActorWithVideo();
@@ -20,76 +18,6 @@ await ImprimeDigitos2();
 
 Console.WriteLine($"Precione cualquier tecla para terminar el programa");
 Console.ReadKey();
-
-
-async Task ImprimeDigitos2()
-{
-    int[] arreglo;
-
-    // Pedir al usuario que ingrese los valores del arreglo
-    Console.Write("Ingrese el tamaño del arreglo: ");
-    int n = int.Parse(Console.ReadLine());
-    arreglo = new int[n];
-
-    for (int i = 0; i < n; i++)
-    {
-        Console.Write("Ingrese el valor para el elemento " + i + ": ");
-        arreglo[i] = int.Parse(Console.ReadLine());
-    }
-
-    // Encontrar el número máximo en el arreglo
-    int maximo = arreglo[0];
-    for (int i = 1; i < n; i++)
-    {
-        if (arreglo[i] > maximo)
-        {
-            maximo = arreglo[i];
-        }
-    }
-
-    // Imprimir el número máximo
-    Console.WriteLine("El número máximo es " + maximo);
-}
-
-async Task ImprimeDigitos()
-{
-
-    int[] Numeros = new int[10];
-
-    Console.WriteLine("Por favor, ingrese 10 números enteros del 1 al 5:");
-
-    // Pedir al usuario que ingrese los valores del arreglo
-    for (int i = 0; i < Numeros.Length; i++)
-    {
-        Console.Write("Número " + (i + 1) + ": ");
-        int num = int.Parse(Console.ReadLine());
-        while (num < 1 || num > 5)
-        {
-            Console.WriteLine("El número debe estar entre 1 y 5. Por favor, ingrese un número válido.");
-            Console.Write("Número " + (i + 1) + ": ");
-            num = int.Parse(Console.ReadLine());
-        }
-        Numeros[i] = num;
-    }
-
-    Console.WriteLine("Histograma del array myArray:");
-
-    // Imprimir un asterisco (*) por cada ocurrencia de cada número en el arreglo
-    for (int i = 1; i <= 5; i++)
-    {
-        Console.Write(i + ": ");
-        for (int j = 0; j < Numeros.Length; j++)
-        {
-            if (Numeros[j] == i)
-            {
-                Console.Write("*");
-            }
-        }
-        Console.WriteLine();
-    }
-
-
-}
 
 async Task MultipleEntitiesQuery()
 {
